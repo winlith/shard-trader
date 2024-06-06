3879c8e675f280fd70e711c61909773226a5b94d
winlith <dariuszfigzal@gmail.com>
2024-06-06 19:13:16 +0200

    installed gh-pages, started api stuff

---
+++ src/apiServices.ts
@@ -0,0 +1,15 @@
+import { ApiMarketResponse } from "./apiTypes";
+import { shards } from "./ids";
+
+export async function getShardPrices(worldId: number) {
+    const idParam = shards.map(shard => shard.id.toString()).join(',');
+    const url = `https://universalis.app/api/v2/${worldId.toString()}/${idParam}?noGst=1`;
+    try {
+        const response = await fetch(url, {method: 'GET'});
+        const json = await response.json() as ApiMarketResponse;
+    }
+    catch
+    {
+        return 
+    }
+}
---
+++ src/ids.ts
@@ -0,0 +1,432 @@
+export const servers = [
+    {
+        name: 'Aether',
+        worlds: [
+            {
+                id: 73,
+                name: 'Adamantoise'
+            },
+            {
+                id: 79,
+                name: 'Cactuar'
+            },
+            {
+                id: 54,
+                name: 'Faerie'
+            },
+            {
+                id: 63,
+                name: 'Gilgamesh'
+            },
+            {
+                id: 40,
+                name: 'Jenova'
+            },
+            {
+                id: 65,
+                name: 'Midgardsormr'
+            },
+            {
+                id: 99,
+                name: 'Sargatanas'
+            },
+            {
+                id: 57,
+                name: 'Siren'
+            }
+        ]
+    },
+    {
+        name: 'Crystal',
+        worlds: [
+            {
+                id: 91,
+                name: 'Balmung'
+            },
+            {
+                id: 34,
+                name: 'Brynhildr'
+            },
+            {
+                id: 74,
+                name: 'Coeurl'
+            },
+            {
+                id: 62,
+                name: 'Diabolos'
+            },
+            {
+                id: 81,
+                name: 'Goblin'
+            },
+            {
+                id: 75,
+                name: 'Malboro'
+            },
+            {
+                id: 37,
+                name: 'Mateus'
+            },
+            {
+                id: 41,
+                name: 'Zalera'
+            }
+        ]
+    },
+    {
+        name: 'Primal',
+        worlds: [
+            {
+                id: 78,
+                name: 'Behemoth'
+            },
+            {
+                id: 93,
+                name: 'Excalibur'
+            },
+            {
+                id: 53,
+                name: 'Exodus'
+            },
+            {
+                id: 35,
+                name: 'Famfrit'
+            },
+            {
+                id: 95,
+                name: 'Hyperion'
+            },
+            {
+                id: 55,
+                name: 'Lamia'
+            },
+            {
+                id: 64,
+                name: 'Leviathan'
+            },
+            {
+                id: 77,
+                name: 'Ultros'
+            }
+        ]
+    },
+    {
+        name: 'Dynamis',
+        worlds: [
+            {
+                id: 406,
+                name: 'Halicarnassus'
+            },
+            {
+                id: 407,
+                name: 'Maduin'
+            },
+            {
+                id: 404,
+                name: 'Marilith'
+            },
+            {
+                id: 405,
+                name: 'Seraph'
+            }
+        ]
+    },
+    {
+        name: 'Chaos',
+        worlds: [
+            {
+                id: 80,
+                name: 'Cerberus'
+            },
+            {
+                id: 83,
+                name: 'Louisoix'
+            },
+            {
+                id: 71,
+                name: 'Moogle'
+            },
+            {
+                id: 39,
+                name: 'Omega'
+            },
+            {
+                id: 401,
+                name: 'Phantom'
+            },
+            {
+                id: 97,
+                name: 'Ragnarok'
+            },
+            {
+                id: 400,
+                name: 'Sagittarius'
+            },
+            {
+                id: 85,
+                name: 'Spriggan'
+            }
+        ]
+    },
+    {
+        name: 'Light',
+        worlds: [
+            {
+                id: 402,
+                name: 'Alpha'
+            },
+            {
+                id: 36,
+                name: 'Lich'
+            },
+            {
+                id: 66,
+                name: 'Odin'
+            },
+            {
+                id: 56,
+                name: 'Phoenix'
+            },
+            {
+                id: 403,
+                name: 'Raiden'
+            },
+            {
+                id: 67,
+                name: 'Shiva'
+            },
+            {
+                id: 33,
+                name: 'Twintania'
+            },
+            {
+                id: 42,
+                name: 'Zodiark'
+            }
+        ]
+    },
+    {
+        name: 'Elemental',
+        worlds: [
+            {
+                id: 90,
+                name: 'Aegis'
+            },
+            {
+                id: 68,
+                name: 'Atomos'
+            },
+            {
+                id: 45,
+                name: 'Carbuncle'
+            },
+            {
+                id: 58,
+                name: 'Garuda'
+            },
+            {
+                id: 94,
+                name: 'Gungnir'
+            },
+            {
+                id: 49,
+                name: 'Kujata'
+            },
+            {
+                id: 72,
+                name: 'Tonberry'
+            },
+            {
+                id: 50,
+                name: 'Typhon'
+            }
+        ]
+    },
+    {
+        name: 'Gaia',
+        worlds: [
+            {
+                id: 43,
+                name: 'Alexander'
+            },
+            {
+                id: 69,
+                name: 'Bahamut'
+            },
+            {
+                id: 92,
+                name: 'Durandal'
+            },
+            {
+                id: 46,
+                name: 'Fenrir'
+            },
+            {
+                id: 59,
+                name: 'Ifrit'
+            },
+            {
+                id: 98,
+                name: 'Ridill'
+            },
+            {
+                id: 76,
+                name: 'Tiamat'
+            },
+            {
+                id: 51,
+                name: 'Ultima'
+            }
+        ]
+    },
+    {
+        name: 'Mana',
+        worlds: [
+            {
+                id: 44,
+                name: 'Anima'
+            },
+            {
+                id: 23,
+                name: 'Asura'
+            },
+            {
+                id: 70,
+                name: 'Chocobo'
+            },
+            {
+                id: 47,
+                name: 'Hades'
+            },
+            {
+                id: 48,
+                name: 'Ixion'
+            },
+            {
+                id: 96,
+                name: 'Masamune'
+            },
+            {
+                id: 28,
+                name: 'Pandaemonium'
+            },
+            {
+                id: 61,
+                name: 'Titan'
+            }
+        ]
+    },
+    {
+        name: 'Meteor',
+        worlds: [
+            {
+                id: 24,
+                name: 'Belias'
+            },
+            {
+                id: 82,
+                name: 'Mandragora'
+            },
+            {
+                id: 60,
+                name: 'Ramuh'
+            },
+            {
+                id: 29,
+                name: 'Shinryu'
+            },
+            {
+                id: 30,
+                name: 'Unicorn'
+            },
+            {
+                id: 52,
+                name: 'Valefor'
+            },
+            {
+                id: 31,
+                name: 'Yojimbo'
+            },
+            {
+                id: 32,
+                name: 'Zeromus'
+            }
+        ]
+    },
+    {
+        name: 'Materia',
+        worlds: [
+            {
+                id: 22,
+                name: 'Bismarck'
+            },
+            {
+                id: 21,
+                name: 'Ravana'
+            },
+            {
+                id: 86,
+                name: 'Sephirot'
+            },
+            {
+                id: 87,
+                name: 'Sophia'
+            },
+            {
+                id: 88,
+                name: 'Zurvan'
+            }
+        ]
+    }
+];
+
+export const shards = [
+    {
+        id: 2,
+        name: 'Fire Shard',
+        seed: {
+            id: 15865,
+            name: 'Firelight Seed'
+        }
+    },
+    {
+        id: 3,
+        name: 'Ice Shard',
+        seed: {
+            id: 15866,
+            name: 'Icelight Seed'
+        }
+    },
+    {
+        id: 4,
+        name: 'Wind Shard',
+        seed: {
+            id: 15867,
+            name: 'Windlight Seed'
+        }
+    },
+    {
+        id: 5,
+        name: 'Earth Shard',
+        seed: {
+            id: 15868,
+            name: 'Earthlight Seed'
+        }
+    },
+    {
+        id: 6,
+        name: 'Lightning Shard',
+        seed: {
+            id: 15869,
+            name: 'Levinlight Seed'
+        }
+    },
+    {
+        id: 7,
+        name: 'Water Shard',
+        seed: {
+            id: 15870,
+            name: 'Waterlight Seed'
+        }
+    }
+];
